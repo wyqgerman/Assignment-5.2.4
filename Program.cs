759fc667339a211b5ca534d9a26e1e9d7384471e
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-12-05 09:11:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Assignment_5._2._4
+{
+    internal class Program
+    {
+        public static bool IsPalindrome(string s, int start, int end)
+        {
+            if (start >= end)
+            {
+                return true;
+            }
+
+            if (s[start] != s[end])
+            {
+                return false;
+            }
+
+            return IsPalindrome(s, start + 1, end - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Input a string: ");
+            string input = Console.ReadLine();
+
+            string formattedInput = input.ToUpper();
+
+            bool result = IsPalindrome(formattedInput, 0, formattedInput.Length - 1);
+
+            if (result)
+            {
+                Console.WriteLine("The string is Palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("The string is not Palindrome.");
+            }
+        }
+    }
+}
